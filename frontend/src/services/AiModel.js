290deb05de061f09a9145d229d8c47587119b55f
GirishChauhan15 <girishchauhan333@mail.com>
2025-05-16 15:29:25 +0530

    Fix - ai model info

--- frontend/src/services/AiModel.js
+++ frontend/src/services/AiModel.js
@@ -10,7 +10,7 @@ export class AiService{
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    model: 'google/gemini-2.0-flash-lite-preview-02-05:free',
+                    model: 'google/gemma-2-9b-it:free',
                     messages: [
                     {
                         role: 'user',
